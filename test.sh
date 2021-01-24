2d52206f987910884f5136f7e923d8275526afa4
Samuel Gélineau <gelisam+github@gmail.com>
2021-01-24 10:08:38 -0500

    print the errors if any

--- test.sh
+++ test.sh
@@ -7,12 +7,14 @@ for x in 0 1 2 3; do
     -i.bak "src/Test$x.hs" 
 done
 
-stack build 2> out
-
-cat out | \
-  tr '\n' '\1' | \
-  sed 's/\[Test\([0-9]\)*\]:Result size of \([^]*\)  = {terms: [0-9]*, types: [0-9]*, coercions: \([0-9]*\)/OUT: \1: \2: \3/g' | \
-  tr '\1' '\n' | \
-  grep '^OUT' | \
-  grep 'Desugar (before optimization)' | \
-  sed 's/^OUT: \([0-9]*\): [^:]*: \([0-9]*\)$/\1: \2 coercions/g'
+if stack build 2> out; then
+  cat out | \
+    tr '\n' '\1' | \
+    sed 's/\[Test\([0-9]\)*\]:Result size of \([^]*\)  = {terms: [0-9]*, types: [0-9]*, coercions: \([0-9]*\)/OUT: \1: \2: \3/g' | \
+    tr '\1' '\n' | \
+    grep '^OUT' | \
+    grep 'Desugar (before optimization)' | \
+    sed 's/^OUT: \([0-9]*\): [^:]*: \([0-9]*\)$/\1: \2 coercions/g'
+else
+  cat out 1>&2
+fi
